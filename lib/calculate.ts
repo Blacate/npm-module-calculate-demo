71b3141d33e074c7360592316ff0d8e6e718a511
Neail <blacated@gmail.com>
2019-01-20 18:02:44 +0800

    init project

---
+++ lib/calculate.ts
@@ -0,0 +1,17 @@
+export class Calculate {
+  plus(x: number, y: number) {
+    return x + y;
+  }
+
+  minus(x: number, y: number) {
+    return x - y;
+  }
+
+  times(x: number, y: number) {
+    return x * y;
+  }
+
+  divide(x: number, y: number) {
+    return x / y;
+  }
+}
